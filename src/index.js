34636e213623179918c4106b9201ae087dcc2045
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2021-04-23 00:41:17 +0200

    Add Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ToDo from './ToDo'
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
 ReactDOM.render(
