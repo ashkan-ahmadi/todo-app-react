fb1ea23e8d7e658e93db61a1e3dc131929be6c98
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2021-04-23 20:45:47 +0200

    Add task to db and fetch all

--- src/ToDo.js
+++ src/ToDo.js
@@ -3,20 +3,35 @@ import NewTaskForm from './components/AddNewTask'
 import PageHeader from './components/PageHeader'
 import Tasks from './components/Tasks'
 import TaskCounter from './components/TaskCounter'
+import Loading from './components/Loading'
+import './spinkit.css'
 
 const ToDo = () => {
   const [tasks, setTasks] = useState([])
   const [taskTitle, setTaskTitle] = useState('')
   const [taskUrgent, setTaskUrgent] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  const fetchTasksfromDatabase = async () => {
+    const dbURL = 'http://localhost:5000/tasks?_sort=id&_order=desc'
+    const request = await fetch(dbURL)
+    const data = await request.json()
+    setTasks(data)
+    setLoading(false)
+  }
+
+  const addTaskToDatabase = async (taskTitle, taskUrgent) => {
+    let request = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ title: taskTitle, urgent: taskUrgent }),
+    }).then((res) => res.json())
+    return request
+  }
 
   useEffect(() => {
-    const fetchTasksfromDatabase = async () => {
-      const dbURL = 'http://localhost:5000/tasks'
-      const request = await fetch(dbURL)
-      const data = await request.json()
-      setTasks(data)
-    }
-
     fetchTasksfromDatabase()
   }, [])
 
@@ -24,35 +39,22 @@ const ToDo = () => {
     e.preventDefault()
 
     if (taskTitle) {
-      const addTaskToDatabase = async (taskTitle, taskUrgent) => {
-        let request = await fetch('http://localhost:5000/tasks', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify({ title: taskTitle, urgent: taskUrgent }),
-        }).then((res) => res.json())
-        // return request
+      setLoading(true) // fetchTasksfromDatabase() wll set it to false when it's done
 
-        console.log(request.id)
-
-        setTasks((prev) =>
-          prev.concat({
-            id: request.id,
-            title: taskTitle,
-            urgent: taskUrgent,
-          })
-        )
-
-        setTaskTitle('')
-        setTaskUrgent(false)
-        e.target[0].focus()
-      }
       addTaskToDatabase(taskTitle, taskUrgent)
+
+      fetchTasksfromDatabase() // this needs to await and run after addTaskToDatabase() but throws an error. You also need to check if addTaskToDatabase() returns success
+
+      setTaskTitle('')
+      setTaskUrgent(false)
+
+      e.target[0].focus()
     } else {
       e.target[0].focus()
     }
   }
+
+  if (loading) return <Loading />
 
   return (
     <>
