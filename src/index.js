0d10d9a58dd5b315b04ddf01dd90945f2643d7c8
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2021-05-03 01:17:59 +0200

    Switch from CSS to SCSS and add FontAwesome

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ToDo from './ToDo'
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+import './assets/css/styles.scss'
 
 ReactDOM.render(
   <React.StrictMode>
